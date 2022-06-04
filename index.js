a27508f0d7b91751c8a74c488a9c0c256736638f
mecenas <jrafael.pfz@gmail.com>
2022-06-04 02:41:18 -0400

    [feature] - set passport.js with google auth strategy & express-session authentication flow

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const session = require('express-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+  session({
+    keys: [keys.cookieKey],
+    secret: keys.cookieSecret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      secure: false, // this should be true only when you don't want to show it for security reason
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/auth')(app);
 
