b3ce232e5e6158d5072d1e5d39a0b3103f5455b5
mecenas <jrafael.pfz@gmail.com>
2022-06-05 17:33:21 -0400

    [feature] - implement stripe payment manager

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -16,13 +17,16 @@ class Header extends Component {
           </li>
         );
       default:
-        return (
+        return [
+          <li>
+            <Payments />
+          </li>,
           <li>
             <a className='red lighten-2' href='/api/logout'>
               Logout
             </a>
-          </li>
-        );
+          </li>,
+        ];
     }
   }
 
--- config/prod.js
+++ config/prod.js
@@ -3,4 +3,6 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   sessionSecret: process.env.SESSION_SECRET,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIP_SECRET_KEY,
 };
