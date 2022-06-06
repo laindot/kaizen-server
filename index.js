209f89ce5358dc6e112ce21096beb46f2511d0d1
mecenas <jrafael.pfz@gmail.com>
2022-06-06 16:48:06 -0400

    fix index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 require('./routes/auth')(app);
 require('./routes/billing')(app);
 
-if (process.env.VERCEL_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file
   app.use(express.static('client/build'));
