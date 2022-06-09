eb5c62fda9f8f996367826eeb3c9f08076be17b3
mecenas <jrafael.pfz@gmail.com>
2022-06-09 17:48:56 -0400

    [feature] - set up mailer with sendgrid

--- routes/surveys.js
+++ routes/surveys.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const Mailer = require('../services/Mailer');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thanks for voting!');
+  });
+
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
     const survey = new Survey({
       title,
@@ -17,5 +23,15 @@ module.exports = (app) => {
       _user: req.user.id,
       dateSent: Date.now(),
     });
+    try {
+      const mailer = new Mailer(survey, surveyTemplate(survey));
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
+    } catch (error) {
+      res.status(422).send(err);
+    }
   });
 };
